3e2e6f4f7be1bdad6041d1b3a1b632411caf8b11
Balpreet Singh Sandhu <singhbalpreet.s.sandhu@gmail.com>
2019-12-22 14:18:06 +1100

    Delete Logic Modal created

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -12,12 +12,36 @@ const PlaceItem = props =>{
   const openMapHandler = () => setShowMap(true);
   const closeMapHander = () => setShowMap(false);
 
+  const [showConfirmModal,setShowConfirmModal]=useState(false);
+
+  const showDeleteWarningHandler = () =>{
+    setShowConfirmModal(true);
+  }
+
+  const cancelDeleteHandler = () =>{
+    setShowConfirmModal(false);
+  }
+
+  const confirmDeleteHandler = () =>{
+    setShowConfirmModal(false);
+    console.log("Delete");
+  }
+
   return (
      <React.Fragment>
        <Modal show={showMap} onCancel={closeMapHander} header={props.address} contentClass="place-item__modal-content" footerClass="place-item__modal-actions" footer={<Button onClick={closeMapHander}>Close</Button>}>
          <div className="map-container">
            <Map center={props.coordinates} zoom={16}/>
          </div>
+       </Modal>
+
+       <Modal show={showConfirmModal} onCancel={cancelDeleteHandler} header="Are You Sure?" footerClass="place-item__modal-actions" footer={
+         <React.Fragment>
+           <Button inverse onClick={cancelDeleteHandler}>Cancel</Button>
+           <Button danger onClick={confirmDeleteHandler}>Delete</Button>
+          </React.Fragment>
+       }>
+         <p>The Delete click will result in deletion of the place which can't be undone!!!!!!</p>
        </Modal>
     <li className="place-item">
       <Card className="place-item__content">
@@ -34,7 +58,7 @@ const PlaceItem = props =>{
         <div className="place-item__actions">
             <Button inverse onClick={openMapHandler}>View on Map</Button>
             <Button to={`/places/${props.id}`}>Edit</Button>
-            <Button danger>Delete</Button>
+            <Button danger onClick={showDeleteWarningHandler}>Delete</Button>
         </div>
       </Card>
   </li>
