4e3326b14422763c944ff17e0d74b7623900d08e
Balpreet Singh Sandhu <singhbalpreet.s.sandhu@gmail.com>
2019-12-22 14:05:39 +1100

    Update Places fixed and working

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
+import Card from '../../shared/components/UIElements/Card';
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
 import './PlaceForm.css';
 import {useForm} from '../../shared/hooks/form-hook';
@@ -37,24 +38,55 @@ const DUMMY_PLACES=[
 
 const UpdatePlace = (props) =>{
     const placeId = useParams().placeId;
-    const identifiedPlace= DUMMY_PLACES.find(p => p.id === placeId);
+    const [isLoading,setIsLoading] = useState(true);
 
-
-    const [formState,inputHandler] = useForm({
+    const [formState,inputHandler,setFormData] = useForm({
       title:{
-        value:identifiedPlace.title,
-        isValid:true
+        value:'',
+        isValid:false
       },
       description:{
-        value:identifiedPlace.description,
-        isValid:true
+        value:'',
+        isValid:false
       }
-    }, true);
+    }, false);
+
+    const identifiedPlace= DUMMY_PLACES.find(p => p.id === placeId);
+
+    useEffect(()=>{
+
+      if(identifiedPlace){
+        setFormData({
+     
+         title:{
+         value:identifiedPlace.title,
+         isValid:true
+        },
+
+         description:{
+         value:identifiedPlace.description,
+         isValid:true
+        }
+
+       },true);
+      }
+     setIsLoading(false);
+  },[setFormData,identifiedPlace]);
 
     if(!identifiedPlace){
         return <div className="center">
+          <Card>
             <h2>Could Not Find any such Place!</h2>
+          </Card>
         </div>
+    }
+
+    if(isLoading){
+      return(
+        <div className="center">
+          <h2>Loading ......</h2>
+        </div>
+      )
     }
 
     const placeUpdateSubmitHandler = event => {
