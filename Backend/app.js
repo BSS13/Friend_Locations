67ef7b29ee68e64e88bf3ffc087ec26200b340f7
Balpreet Singh Sandhu <singhbalpreet.s.sandhu@gmail.com>
2019-12-24 15:46:29 +1100

    Users specific Routes created and tested

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,14 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const placesRoutes=require('./routes/places-routes');
+const usersRoutes=require('./routes/users-routes');
 const HttpError = require('./models/http-error');
 
 const app=express();
 
 app.use(bodyParser.json());
 app.use("/api/places",placesRoutes);
+app.use("/api/users",usersRoutes);
 
 app.use((req,res,next)=>{
     const error = new HttpError('Could Not Find This Route',404);
---
+++ Backend/controllers/users-controllers.js
@@ -0,0 +1,52 @@
+const HttpError= require('../models/http-error');
+const uuid = require('uuid/v4');
+
+const DUMMY_USERS = [
+    {
+        id:'u1',
+        name:'Balpreet',
+        email:'b@b.com',
+        password:'testers'
+    }
+];
+
+//Route to fetch all the Users
+const getUsers = (req,res,next)=>{
+    res.json({users:DUMMY_USERS});
+}
+
+//Route to complete the Signup
+const signup = (req,res,next) =>{
+    const {name,email,password} = req.body;
+
+    if(DUMMY_USERS.find(u=> u.email === email)){
+        throw new HttpError('Could not create a User, email is already taken',422);
+    };
+
+    const createdUser = {
+        id:uuid(),
+        name,
+        email,
+        password
+    };
+
+    DUMMY_USERS.push(createdUser);
+    res.status(201).json({user:createdUser});
+}
+
+//Route to complete the Login
+const login = (req,res,next) =>{
+    const {email,password} = req.body;
+
+    const identifiedUser = DUMMY_USERS.find(u => u.email === email);
+
+    if(!identifiedUser || identifiedUser.password !== password){
+        throw new HttpError("Invalid Credentials, Could not Identify the User",401)
+    };
+
+    res.json({msg:'Logged In'});
+}
+
+exports.getUsers=getUsers;
+exports.signup=signup;
+exports.login=login;
