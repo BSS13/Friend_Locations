071a51896e87af542149234c0331b9fcc66d6dce
Balpreet Singh Sandhu <singhbalpreet.s.sandhu@gmail.com>
2019-12-27 19:00:35 +1100

    CORS implemented

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,14 @@ const app=express();
 //Middleware to parse the incoming Request Body
 app.use(bodyParser.json());
 
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','origin,X-Requested-With,Content-Type,Accept,Authorization');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,DELETE');
+
+    next();
+})
+
 //Middleware to connect the Specific routes to associated things
 app.use("/api/places",placesRoutes);
 app.use("/api/users",usersRoutes);
