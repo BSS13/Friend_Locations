301b619a620e8fe35ed578eb512cafd1a65b66cb
Balpreet Singh Sandhu <singhbalpreet.s.sandhu@gmail.com>
2020-01-01 15:00:48 +1100

    Backend Image Upload Handling module using multer added

--- Backend/routes/users-routes.js
+++ Backend/routes/users-routes.js
@@ -2,9 +2,10 @@ const express=require('express');
 const router=express.Router();
 const usersController=require('../controllers/users-controllers');
 const {check} = require('express-validator');
+const fileUpload = require('../middleware/file-upload');
 
 router.get("/",usersController.getUsers);
-router.post("/signup",[
+router.post("/signup", fileUpload.single('image'),[
   check('name').not().isEmpty(),
   check('email').normalizeEmail().isEmail(),
   check('password').isLength({min:6})
