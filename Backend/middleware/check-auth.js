7592698bd730f92615907beaf1268276c716e4d3
Balpreet Singh Sandhu <singhbalpreet.s.sandhu@gmail.com>
2020-01-02 22:53:37 +1100

    Middleware to accept incoming Tokens created

---
+++ Backend/middleware/check-auth.js
@@ -0,0 +1,19 @@
+const HttpError = require('../models/http-error');
+const jwt = require('jsonwebtoken');
+
+module.exports = (req,res,next) =>{
+    try{
+    const token = req.headers.authorization.split(' ')[1];
+
+    if(!token){
+        throw new Error('Authentication Failed');
+    }
+    
+    const decodedToken = jwt.verify(token,'supersecret_not_to_be_shared');
+    req.userData = {userId: decodedToken.userId};
+    next();
+    }catch(err){
+        const error = new HttpError('Authentication Failed',401);
+        return next(error);
+    }
+};
--- Backend/routes/places-routes.js
+++ Backend/routes/places-routes.js
@@ -3,10 +3,13 @@ const router=express.Router();
 const placesController = require('../controllers/places-controllers');
 const {check} = require('express-validator');
 const fileUpload = require('../middleware/file-upload');
+const checkAuth = require('../middleware/check-auth');
 
 router.get("/:pid",placesController.getPlaceById);
 
 router.get("/user/:uid",placesController.getPlacesByUserId);
+
+router.use(checkAuth);
 
 router.post("/", fileUpload.single('image'),[
     check('title').not().isEmpty(),
